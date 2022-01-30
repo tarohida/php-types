fa5f427c1281d7db9dc9214e17b97af1e9cc82e4
taro-hida <sk8trou@gmail.com>
2022-01-30 10:57:30 +0900

    esa708 `Url` の型を作成

---
+++ src/TaroHida/Types/Url.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+
+namespace TaroHida\Types;
+
+use InvalidArgumentException;
+
+/*
+ * (c) Claude Khedhiri <claude@khedhiri.com>
+ * (c) Taro Hida <sk8trou@gmail.com>
+ * originate from https://github.com/ck-developer/php-types/blob/c72f6fa0051a20552cfe7d5b88936ded2d2dc2f8/src/Url.php
+ */
+class Url
+{
+    /**
+     * @var string
+     */
+    private string $value;
+
+    /**
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        if (!self::isValid($value)) {
+            throw new InvalidArgumentException("${value} is not a valid url");
+        }
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return bool
+     */
+    public static function isValid(string $value): bool
+    {
+        return (bool)filter_var($value, FILTER_VALIDATE_URL);
+    }
+
+}
---
+++ tests/TaroHida/Types/UrlTest.php
@@ -0,0 +1,63 @@
+<?php
+/** @noinspection HttpUrlsUsage */
+/** @noinspection NonAsciiCharacters */
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpDocMissingThrowsInspection */
+/** @noinspection PhpPrivateFieldCanBeLocalVariableInspection */
+/** @noinspection PhpUnhandledExceptionInspection */
+declare(strict_types=1);
+
+namespace Tests\TaroHida\Types;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use TaroHida\Types\Url;
+
+/*
+ * (c) Claude Khedhiri <claude@khedhiri.com>
+ * (c) Taro Hida <sk8trou@gmail.com>
+ * originate from https://github.com/ck-developer/php-types/blob/c72f6fa0051a20552cfe7d5b88936ded2d2dc2f8/tests/UrlTest.php
+ */
+final class UrlTest extends TestCase
+{
+    /**
+     * @param string $url
+     *
+     * @dataProvider validValues
+     */
+    public function testValid(string $url)
+    {
+        $this->assertSame($url, (new Url($url))->getValue());
+    }
+
+    /**
+     * @param string $url
+     *
+     * @dataProvider invalidValues
+     */
+    public function testInvalid(string $url)
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Url($url);
+    }
+
+    public function validValues(): array
+    {
+        return [
+            ['http://localhost'],
+            ['http://example.com'],
+            ['https://example.com'],
+        ];
+    }
+
+    public function invalidValues(): array
+    {
+        return [
+            [''],
+            ['localhost'],
+            ['http://'],
+        ];
+    }
+}
+
